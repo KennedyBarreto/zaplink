131f3772629834639b063f6dd650d4fd0937287e
Kennedy Barreto <kennedygcbarreto@hotmail.com>
2023-12-04 15:39:44 -0300

    fix: fix navbar css & closing after click outside

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,34 +3,36 @@ import {FaBars, FaCaretLeft} from "react-icons/fa";
 
 
 export default function Nav(){
-    // const body = document.getElementById("main");
-
-    
-    // Mobile Navigation setup
-    function showHideNav(){
-        const hamburger = document.getElementById("hamburger") 
+    function showHideNav() {
+        const hamburger = document.getElementById("hamburger");
         const closeNav = document.getElementById("closeNav");
         const mNav = document.getElementById("mNav");
-        if(mNav.style.display !== "block"){
-            mNav.style.display = "block"
-            closeNav.style.display = "block"
-            hamburger.style.display = "none"
-        }else{
-            mNav.style.display = "none"
-            closeNav.style.display = "none"
-            hamburger.style.display = "block"
-
+      
+        if (mNav.style.display !== "block") {
+          mNav.style.display = "block";
+          closeNav.style.display = "block";
+          hamburger.style.display = "none";
+          // Adicione outras lógicas necessárias
+        } else {
+          mNav.style.display = "none";
+          closeNav.style.display = "none";
+          hamburger.style.display = "block";
+          // Adicione outras lógicas necessárias
         }
-        
-    }
-    // Hide Nav on body click
-    // function bodyHide(){
-    //     if(mNav.style.display == "block"){
-    //         mNav.style.display = "none"
-    //     }
-    // }
-    // body.addEventListener("click", bodyHide);
-// Function end
+      }
+      
+      // Fecha a navegação móvel se o clique for fora dela
+      document.addEventListener("click", function(event) {
+        const mNav = document.getElementById("mNav");
+        const hamburger = document.getElementById("hamburger");
+      
+        if (!mNav.contains(event.target) && event.target !== hamburger) {
+          mNav.style.display = "none";
+          hamburger.style.display = "block"; // Mostra o hamburger quando a navegação é fechada
+          // Adicione outras lógicas necessárias
+        }
+      });
+
 
 const scrollToForm = () => {
     const formSection = document.getElementById('form-data');
