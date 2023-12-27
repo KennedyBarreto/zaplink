2f727a7b4e0023a323b75af669a45f31b6aa91d0
Kennedy Barreto <kennedygcbarreto@hotmail.com>
2023-12-27 10:38:47 -0300

    commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,26 +1,22 @@
 import React from "react";
-import {HashLink} from "react-router-hash-link"
-export default function Footer(){
-    return (
-        <footer>
-            <div id="footer">
-                <div>
-                    
-                <img src=""></img>
-                <img src=""></img>
-               
-                </div>
-            </div>
-            <div className="endNote">
-            <div>
-    <p>COMHUB 2023 &copy; Todos os direitos reservados</p>
-</div>
-<div>
-    <p>Política de Privacidade. Termos e Condições. Documentação.</p>
-</div>
-
-
-            </div>
-        </footer>
-    )
+import { HashLink } from "react-router-hash-link";
+export default function Footer() {
+  return (
+    <footer>
+      <div id="footer">
+        <div>
+          <img src="#"></img>
+          <img src=""></img>
+        </div>
+      </div>
+      <div className="endNote">
+        <div>
+          <p>COMHUB 2023 &copy; Todos os direitos reservados</p>
+        </div>
+        <div>
+          <p>Política de Privacidade. Termos e Condições. Documentação.</p>
+        </div>
+      </div>
+    </footer>
+  );
 }
