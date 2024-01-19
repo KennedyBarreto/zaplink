432b8e4f1617c58559ce1d7b19ec458418a9ec92
Kennedy Barreto <99549329+KennedyBarreto@users.noreply.github.com>
2024-01-19 16:19:22 -0300

    feat: changed footer content

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,17 +1,19 @@
 import React from "react";
-import { HashLink } from "react-router-hash-link";
 export default function Footer() {
   return (
     <footer>
-      <div id="footer">
-        <div>
-          <img src="#"></img>
-          <img src=""></img>
-        </div>
-      </div>
+      <div id="footer"></div>
       <div className="endNote">
         <div>
-          <p>COMHUB 2023 &copy; Todos os direitos reservados</p>
+          Feito com ❤️ por{" "}
+          <a
+            href="https://github.com/KennedyBarreto"
+            target="_blank"
+            rel="noreferrer"
+            s
+          >
+            Kennedy Barreto
+          </a>
         </div>
         <div>
           <p>Política de Privacidade. Termos e Condições. Documentação.</p>
