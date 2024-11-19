29419ab10010f1254d00a81237a77f3002b205ce
Kennedy Barreto <99549329+KennedyBarreto@users.noreply.github.com>
2024-11-19 12:36:36 -0300

    refactor: add base typescript index file

---
+++ server/src/index.ts
@@ -0,0 +1,119 @@
+import dotenv from "dotenv";
+import express, { Request, Response } from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import shortid from "shortid";
+import Url from "./Url";
+import * as utils from "./Util/util";
+
+// configure dotenv
+dotenv.config();
+const app = express();
+
+interface UrlRequest {
+  origUrl: string;
+  shortTitle?: string;
+}
+
+// cors configuration
+const corsOptions: cors.CorsOptions = {
+  origin: "*",
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+
+// Database connection
+mongoose
+  .connect(process.env.MONGO_URI as string, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log(`Db Connected`);
+  })
+  .catch((err: Error) => {
+    console.log(err.message);
+  });
+
+// URL shortener endpoint
+app.post("/short", async (req: Request<{}, {}, UrlRequest>, res: Response) => {
+  console.log("HERE", req.body.url);
+  const { origUrl, shortTitle } = req.body;
+  const base = process.env.DOMAIN_URL as string;
+  let urlId: string;
+
+  if (!shortTitle) {
+    urlId = shortid.generate();
+    console.log(urlId);
+  } else {
+    urlId = shortTitle;
+    console.log(urlId);
+  }
+
+  if (utils.validateUrl(origUrl)) {
+    try {
+      let url = await Url.findOne({ origUrl });
+      if (url) {
+        console.log("Duplicada");
+        return res.status(200).json(url);
+      } else {
+        const shortUrl = `${base}/${urlId}`;
+        url = new Url({
+          origUrl,
+          shortUrl,
+          urlId,
+          date: new Date(),
+        });
+
+        await url.save();
+        res.json(url);
+      }
+    } catch (err) {
+      console.log(err);
+      res.status(500).json("Server Error");
+    }
+  } else {
+    res.status(400).json("Invalid Original Url");
+  }
+});
+
+// redirect endpoint
+app.get("/:urlId", async (req: Request, res: Response) => {
+  try {
+    const url = await Url.findOne({ urlId: req.params.urlId });
+
+    if (url) {
+      url.clicks++;
+      url.save();
+      return res.redirect(url.origUrl);
+    } else res.status(404).json("Not found");
+  } catch (err) {
+    console.log(err);
+    res.status(500).json("Server Error");
+  }
+});
+
+// check url endpoint
+app.post("/check-url", async (req: Request, res: Response) => {
+  const { urlId } = req.body;
+
+  try {
+    const existingUrl = await Url.findOne({ urlId });
+
+    if (existingUrl) {
+      return res.status(400).json({ error: "URL já existe" });
+    }
+
+    return res.status(200).json({ message: "URL disponível" });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: "Erro interno do servidor" });
+  }
+});
+
+const PORT = process.env.PORT || 3333;
+app.listen(PORT, () => {
+  console.log(`Server is running at PORT ${PORT}`);
+});
