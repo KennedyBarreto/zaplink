139faf48fd1c5941e19cbe076c46a57e83a4f430
Kennedy Barreto <99549329+KennedyBarreto@users.noreply.github.com>
2024-11-19 12:28:36 -0300

    refactor: moved server files to src folder

--- server/src/index.js
+++ server/src/index.js
@@ -78,7 +78,7 @@ app.post("/short", async (req, res) => {
 app.get("/:urlId", async (req, res) => {
   try {
     const url = await Url.findOne({ urlId: req.params.urlId });
-    
+
     if (url) {
       url.clicks++;
       url.save();
